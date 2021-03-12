14bad2a061c36ef1b5c115629eba3a533c871511
Rishi Khurana <rishikhurana.sufi@gmail.com>
2021-03-12 11:05:21 +0530

    Pushing test cases

--- src/main/helloworld.py
+++ src/main/helloworld.py
@@ -2,8 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class HelloWorld():
+
 @app.route('/')
 def index():
-    return 'Hello to World with Python Flask!'
+    return 'Hello to Participants using Python Flask!'
 
-app.run(host='0.0.0.0', port=8083)
+app.run(host='0.0.0.0', port=8082)
---
+++ src/test/hello_world_test.py
@@ -0,0 +1,7 @@
+import pytest
+
+from helloWorld import HelloWorld
+
+def test_index_validation():
+    helloWorld = HelloWorld()
+    assert wallet.index == "Hello to Participants using Python Flask!"
