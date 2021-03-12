ea42e64609b8ca0314fb9517c288a2548193c151
ajithreddy444 <32986843+ajithreddy444@users.noreply.github.com>
2021-03-12 12:39:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Congratulations, it's a Python web app!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8082, debug=True)
