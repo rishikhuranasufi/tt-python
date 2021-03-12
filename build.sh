0fbf5da64ebd16e4979cce34a81563dbc383bbb9
Rishi Khurana <rishikhurana.sufi@gmail.com>
2021-03-12 14:22:31 +0530

    Pushing files

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ sudo apt install python3-pip python3-venv -y
 pip3 -V
 python3 -m venv venv
 . ./venv/bin/activate
-python3 -m pip3 install -r requirements.txt
+python3 -m pip install -r requirements.txt
 python3 pytest hello_world.py
