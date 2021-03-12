3bce2c3063c42bb1e952a1e21801e5869780d595
Rishi Khurana <rishikhurana.sufi@gmail.com>
2021-03-12 14:12:09 +0530

    Pushing a test case

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 
 sudo apt-get update
 sudo apt install python3-pip python3-venv -y
-pip3 -V     
+pip3 -V
+python3 -m venv venv
+. ./venv/bin/activate
+python3 -m pip install -r requirements.txt
+python3 pytest hello_world.py
---
+++ hello_world.py
@@ -0,0 +1,7 @@
+import pytest
+
+from helloWorld import HelloWorld
+
+def test_index_validation():
+    helloWorld = HelloWorld()
+    assert wallet.index == "Congratulations, it's a Python web app!"
