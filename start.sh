4623714bd68405974318edca01f326e00f41fbcb
ajithreddy444 <32986843+ajithreddy444@users.noreply.github.com>
2021-03-12 13:49:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 
 python3 -m venv venv
 . ./venv/bin/activate
-sudo python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
+nohup python3 main.py > ~/flasklogs.log 2>&1 &
