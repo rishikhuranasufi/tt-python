10e7e1eb1ae2dc723a745435600216264c878bec
ajithreddy444 <32986843+ajithreddy444@users.noreply.github.com>
2021-03-12 13:04:19 +0530

    Delete src directory

--- src/test/hello_world_test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from helloWorld import HelloWorld
-
-def test_index_validation():
-    helloWorld = HelloWorld()
-    assert wallet.index == "Hello to Participants using Python Flask!"
