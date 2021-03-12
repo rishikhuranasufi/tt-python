bd655a0ae463461f290075d91a11fbd501a40e25
Rishi Khurana <rishikhurana.sufi@gmail.com>
2021-03-12 14:39:26 +0530

    Pushing files

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 class HelloWorld():
 
-@app.route("/")
-def index():
-    return "Congratulations, it's a Python web app!"
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8082, debug=True)
+    @app.route("/")
+    def index():
+        return "Congratulations, it's a Python web app!"
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=8082, debug=True)
